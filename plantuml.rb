1bc9c54f07e000b7560d2ae9a8c929d65e6f1640
YJ Park <yjpark@gmail.com>
2012-04-28 00:20:59 +0800

    implementation, not tested yet

--- plantuml.rb
+++ plantuml.rb
@@ -16,8 +16,23 @@ module Jekyll
     def render(context)
       output = super
       code = super.join
+      puts code
+
+      site = context.registers[:site]
+      folder = "/images/plantuml/"
+      filepath = folder + Digest::MD5.hexdigest(code) + ".png"
+      plantuml_jar = File.expand_path(site.config['plantuml_jar'])
+      cmd = "java -jar " + plantuml_jar + " -pipe > " + File.join(site.dest, filepath)
+      puts "Generate PlantUML image: \n\t" + cmd
+      exec "mkdir -p " + site.dest + folder
+      stdin, stdout, stderr = Open3.popen3('java -jar plantuml.jar -pipe > somefile.png ')
+      stdin.puts(code)
+      stdin.close()
+      result = stdout.gets
+      puts "\t -> " + result
+
       source = "<figure class='code'>"
-      source += code
+      source += "<img src='" + filepath + "'>"
       source += "</figure>"
       source
     end
